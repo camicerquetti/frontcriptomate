661b832befc300cc1a8f099e923887762979820a
camicerquetti <camilacerquetti18@gmail.com>
2024-09-05 16:27:19 -0300

    footer

--- src/componentes/footer.js
+++ src/componentes/footer.js
@@ -1,7 +1,6 @@
-// src/components/Footer.js
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom'; // Renombramos el Link de react-router-dom
-import { Box, Typography, Link as MuiLink, Container, Grid } from '@mui/material'; // Renombramos el Link de MUI a MuiLink
+import { Link as RouterLink } from 'react-router-dom'; // Usa RouterLink para redirección interna
+import { Box, Typography, Link as MuiLink, Container, Grid } from '@mui/material'; 
 import { Facebook, Twitter, Instagram } from '@mui/icons-material';
 
 const Footer = () => {
@@ -13,7 +12,6 @@ const Footer = () => {
         color: '#fff',
         padding: '20px 0',
         marginTop: 'auto',
-        
       }}
     >
       <Container>
@@ -24,7 +22,7 @@ const Footer = () => {
             </Typography>
             <ul style={{ listStyle: 'none', padding: 0 }}>
               <li><RouterLink to="/nosotros" style={{ color: 'inherit', textDecoration: 'none' }}>Sobre nosotros</RouterLink></li>
-              <li><MuiLink href="/contacto" color="inherit" underline="hover">Contáctanos</MuiLink></li>
+              <li><RouterLink to="/contacto" style={{ color: 'inherit', textDecoration: 'none' }}>Contáctanos</RouterLink></li>
             </ul>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
@@ -42,8 +40,8 @@ const Footer = () => {
               Legal
             </Typography>
             <ul style={{ listStyle: 'none', padding: 0 }}>
-              <li><MuiLink href="/terminos" color="inherit" underline="hover">Términos y condiciones</MuiLink></li>
-              <li><MuiLink href="/politicas" color="inherit" underline="hover">Política de privacidad</MuiLink></li>
+              <li><RouterLink to="/terminos" style={{ color: 'inherit', textDecoration: 'none' }}>Términos y condiciones</RouterLink></li>
+              <li><RouterLink to="/politicas" style={{ color: 'inherit', textDecoration: 'none' }}>Política de privacidad</RouterLink></li>
             </ul>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
@@ -51,13 +49,13 @@ const Footer = () => {
               Síguenos
             </Typography>
             <Box sx={{ display: 'flex', gap: 2 }}>
-              <MuiLink href="#" color="inherit">
+              <MuiLink href="https://facebook.com" color="inherit" target="_blank" rel="noopener noreferrer">
                 <Facebook />
               </MuiLink>
-              <MuiLink href="#" color="inherit">
+              <MuiLink href="https://twitter.com" color="inherit" target="_blank" rel="noopener noreferrer">
                 <Twitter />
               </MuiLink>
-              <MuiLink href="#" color="inherit">
+              <MuiLink href="https://instagram.com" color="inherit" target="_blank" rel="noopener noreferrer">
                 <Instagram />
               </MuiLink>
             </Box>
